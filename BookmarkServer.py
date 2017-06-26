21c7d230738d07565982f16c45e48c9009bc4783
Katyanna Moura <amelie.kn@gmail.com>
2017-06-26 18:29:42 -0300

    Checking in my bookmar server

---
+++ BookmarkServer.py
@@ -0,0 +1,79 @@
+import http.server
+import requests
+from urllib.parse import unquote, parse_qs
+
+memory = {}
+
+form = '''
+<!DOCTYPE html>
+<title>Bookmark Server</title>
+<form method="POST">
+    <label>Long URI:
+        <input name="longuri">
+    </label>
+    <br>
+    <label>Short name:
+        <input name="shortname">
+    </label>
+    <br>
+    <button type="submit">Save it!</button>
+</form>
+<p>URIs I know about:
+<pre>
+{}
+</pre>
+'''
+
+def CheckURI(uri, timeout=5):
+    try:
+        r = requests.get(uri, timeout=timeout)
+        return r.status_code == 200
+    except requests.RequestException:
+        return False
+
+class Shortener(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        name = unquote(self.path[1:])
+
+        if name:
+            if name in memory:
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain; charset=utf-8')
+                self.end_headers()
+                self.wfile.write("I don't know '{}'.".format(name).encode())
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            known = "\n".join("{} : {}".format(key, memory[key])
+                              for key in sorted(memory.keys()))
+            self.wfile.write(form.format(known).encode())
+
+    def do_POST(self):
+        length = int(self.headers.get('Content-length', 0))
+        body = self.rfile.read(length).decode()
+        params = parse_qs(body)
+        longuri = params["longuri"][0]
+        shortname = params["shortname"][0]
+
+        if CheckURI(longuri):
+            memory[shortname] = longuri
+
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(
+                "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd.serve_forever()
